01f68f743916e609265f8ec5de8e5b5fefadd1ce
jquigley1003 <jeff.quigley@wavinghi.com>
2014-11-19 08:43:04 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nextSetYeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nextSetYeomanApp
+ */
+angular.module('nextSetYeomanApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
